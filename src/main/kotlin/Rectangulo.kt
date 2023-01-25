41af49d923835ad91ac81946ecd89b91ba3d0784
Cemaik <cesarserranobenitez@gmail.com>
2023-01-25 19:27:53 +0100

    Inicio

---
+++ src/main/kotlin/Rectangulo.kt
@@ -0,0 +1,13 @@
+class Rectangulo {
+    var base: Int = 5
+    var altura: Int = 2
+    fun calcularArea() {
+        val area = base * altura
+    }
+    fun calcularPerimitro() {
+        val perimetro = 2 * base + 2 * altura
+    }
+    fun mostrar(){
+        println("")
+    }
+}
---
+++ src/main/kotlin/rectangulo1.kt
@@ -0,0 +1,11 @@
+var base: Int = 5
+var altura: Int = 2
+fun calcularArea() {
+    val area = base * altura
+}
+fun calcularPerimitro() {
+    val perimetro = 2 * base + 2 * altura
+}
+fun imprimir(){
+
+}
